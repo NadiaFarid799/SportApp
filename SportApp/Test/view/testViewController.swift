8af3e51fa676eaf4d52f2c187ccebf841f6af2c2
nadia <nadianody@gmail.com>
2025-05-15 14:23:21 -0700

    Initial Commit

---
+++ SportApp/Test/view/testViewController.swift
@@ -0,0 +1,31 @@
+//
+//  testViewController.swift
+//  SportApp
+//
+//  Created by NadiaFarid on 15/05/2025.
+//
+
+import UIKit
+
+class testViewController: UIViewController {
+
+    @IBOutlet weak var test_btn: UIButton!
+    @IBOutlet weak var test_label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
