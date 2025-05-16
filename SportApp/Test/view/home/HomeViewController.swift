0e77f73d6c322d4f8adca56a0a32577fba37b3e5
zeyadmamoun <zeyadmamoun952>
2025-05-16 16:38:14 +0300

    Added Test Navigation Code

---
+++ SportApp/Test/view/home/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  SportApp
+//
+//  Created by Macos on 16/05/2025.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+    
+    @IBAction func goToNextPage(_ sender: Any) {
+        
+        
+    }
+    
+}
--- SportApp/Test/view/testViewController.swift
+++ SportApp/Test/view/testViewController.swift
@@ -16,16 +16,11 @@ class testViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    @IBAction func goToNext(_ sender: Any) {
+        let myVC = HomeViewController(nibName: "HomeViewController", bundle: nil)
+        self.navigationController?.pushViewController(myVC, animated: true)
     }
-    */
-
+    
 }
