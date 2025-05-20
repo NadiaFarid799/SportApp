60b29d95d4a79b2e2c6cd99ff1e15930bc069e68
zeyadmamoun <zeyadmamoun952>
2025-05-20 18:36:55 +0300

    added leagues tableView

--- SportApp/leagues/view/LeaguesViewController.swift
+++ SportApp/leagues/view/LeaguesViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class LeaguesViewController: UIViewController {
+class LeaguesViewController: UIViewController,UITableViewDataSource,UITableViewDelegate{
     
     var sportName: String?
     var remoteService: RemoteService?
-
+    var leagues : [League] = []
+    
+    @IBOutlet weak var leaguesTableView: UITableView!
+    
     override func viewDidLoad(){
         super.viewDidLoad()
+        
+        //MARK: configure the tableView
+        leaguesTableView.dataSource = self
+        leaguesTableView.delegate = self
+        
+        //MARK: load the League cell NibFile
+        let nib = UINib(nibName: "LeaguesViewCell", bundle: nil)
+        self.leaguesTableView.register(nib, forCellReuseIdentifier: "leagueCell")
         
         // MARK: checking if the sport name sent from previous page is null or not
         guard let sportName = sportName else {
@@ -40,12 +51,36 @@ class LeaguesViewController: UIViewController {
         remoteService!.fetchData(url: leaguesUrl) { (result: Result<LeagueResponse,Error>) in
             switch result {
             case .success(let response):
-                print(response.result.count)
-                
+                self.leagues = response.result
+                self.leaguesTableView.reloadData()
             case .failure(let error):
                 print(error)
             }
         }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        leagues.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80.0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "leagueCell", for: indexPath) as? LeaguesViewCell else {
+            print("not detected")
+            return LeaguesViewCell()
+        }
+        
+        let league = leagues[indexPath.row]
+        
+        cell.leagueName.text = league.league_name
+        cell.leagueCountry.text = league.country_name
+        
+        
+        return cell
     }
     
 
